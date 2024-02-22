65d9ce3338aaf6684d629493a6075c72abf5c8e4
Mohanraj49 <mohan13me575749@gmail.com>
2024-02-22 17:09:32 +0530

    Dubai

--- Naveen.java
+++ Naveen.java
@@ -5,7 +5,10 @@ public class Naveen {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
+		for(int i=5 ; i<52 ; i++)
+		{
+			System.out.println(i);
+		}
 	}
 
 }
